4f6f48d84b7acd9281dcd8a8e688978569c96f92
Laytdesu <134925653+Laytdesu@users.noreply.github.com>
2026-02-15 15:28:33 +0200

    Added placeholder YAML creation
    Signed-off-by: Laytdesu <134925653+Laytdesu@users.noreply.github.com>

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows.Input;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
@@ -39,7 +40,54 @@ public partial class MainWindow : Window
 
     private string BuildYamlContent()
     {
-        return "Placeholder";
+        var sb = new StringBuilder();
+        sb.AppendLine("speakers:");
+        foreach (var speaker in SpeakersCollection)
+        {
+            sb.AppendLine("  - id: " + speaker.Id);
+            sb.AppendLine("  name: " + speaker.Name);
+            sb.AppendLine("  path: " + speaker.SpkPath);
+            sb.AppendLine("  dict: " + speaker.Dict);
+        }
+        sb.AppendLine();
+
+        sb.AppendLine("dictionaries:");
+        foreach (var dict in DictsCollection)
+        {
+            sb.AppendLine("  - name: " + dict.Name);
+            sb.AppendLine("  path: " + dict.DictPath);
+        }
+        sb.AppendLine();
+
+        sb.AppendLine("embeds:");
+        foreach (var embed in EmbedsConfigCollection)
+        {
+            sb.AppendLine("  - duration: " + embed.Duration);
+            sb.AppendLine("  pitch: " + embed.Pitch);
+            sb.AppendLine("  energy: " + embed.Energy);
+            sb.AppendLine("  breathiness: " + embed.Breath);
+            sb.AppendLine("  voicing: " + embed.Voicing);
+            sb.AppendLine("  tenstion: " + embed.Tension);
+            sb.AppendLine("  type: " + embed.Type);
+            sb.AppendLine("  pe: " + embed.PitchExtract);
+            sb.AppendLine("  hnsep: " + embed.Hnsep);
+        }
+
+        sb.AppendLine();
+
+        sb.AppendLine("other:");
+        foreach (var otherItem in OtherConfigCollection)
+        {
+            sb.AppendLine("  - saveevery: " + otherItem.SaveEvery);
+            sb.AppendLine("  maxstepcount: " + otherItem.MaxSteps);
+            sb.AppendLine("  maxkeepckpt: " + otherItem.MaxKeepCheckpoint);
+            sb.AppendLine("  startpermanentckpt: " + otherItem.StartPermCheckpoint);
+            sb.AppendLine("  savepermanentckpt: " + otherItem.SavePermCheckpoint);
+            sb.AppendLine("  maxbatch: " + otherItem.MaxBatchSize);
+            sb.AppendLine("  output: " + otherItem.OutputPath);
+        }
+
+        return sb.ToString();
     }
 }
 
